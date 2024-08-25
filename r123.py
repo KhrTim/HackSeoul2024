a773ef9a4c642c546e188dcb614573ae6ea60edd
Yeeachan <qkrd1285@cau.ac.kr>
2024-08-25 10:49:47 +0900

    comcom

--- r123.py
+++ r123.py
@@ -6,8 +6,8 @@ def process_category(category):
         return "https://www.coupang.com/vp/products/1414809213?itemId=2779751047&vendorItemId=5484609359&sourceType=CATEGORY&categoryId=486148&isAddedCart="
     elif category == "Man":
         return "https://www.coupang.com/vp/products/5291011072?itemId=7607262414&vendorItemId=81735052457&q=%EB%82%A8%EC%9E%90%EC%98%B7&itemsCount=36&searchId=95d888e621424a67bdbe9ef681bc1aa8&rank=19&isAddedCart="
-    elif category == "Mouse":
-        return "https://www.coupang.com/vp/products/8145067451?itemId=19848126302&vendorItemId=70778258892&pickType=COU_PICK&q=%EB%A7%88%EC%9A%B0%EC%8A%A4&itemsCount=36&searchId=c102d3773d894c31871b1201498c685d&rank=1&isAddedCart="
+    elif category == "Keyboard":
+        return "https://www.coupang.com/vp/products/132687601?itemId=390196810&vendorItemId=3335653163&q=%ED%82%A4%EB%B3%B4%EB%93%9C&itemsCount=36&searchId=702a000fd0384a6f934fb34480007db6&rank=3"
     return None
 
 def search_query(searchQuery):
